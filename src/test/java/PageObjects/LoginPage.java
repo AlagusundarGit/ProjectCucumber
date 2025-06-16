926afde1cb7c7f9fef78efc05f1ac614d9991b88
AlagusundarGit <asunda465@gmail.com>
2025-06-16 15:57:06 +0530

    test

--- src/test/java/PageObjects/LoginPage.java
+++ src/test/java/PageObjects/LoginPage.java
@@ -1,11 +1,9 @@
 package PageObjects;
 
 public class LoginPage {
-<<<<<<< HEAD
 
-=======
   public void test() {
 		System.out.println("test conflict");
 	}
->>>>>>> c73ac391f790c850ed4354d70fd229e56f075ed9
+
 }
